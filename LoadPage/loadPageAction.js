56250105c0f5a351886db5a7482fb974d56df6ae
isuru0 <isurushehara@gmail.com>
2023-06-07 22:11:46 +0530

    Add Password function (without Shutdown)

--- LoadPage/loadPageAction.js
+++ LoadPage/loadPageAction.js
@@ -1,3 +1,26 @@
+var Error = document.getElementById("error");
+    Error.style.display = "none";
+
 function password(){
-    location.href = "file:///D:/OFFICIAL/Website/Project/04%20Windows%20XP/index.html";
+    var passw = document.getElementById("password").value;
+    if (passw === "password"){
+        location.href = "file:///D:/OFFICIAL/Website/Project/04%20Windows%20XP/index.html";
+    }
+    else if (passw === "0"){
+        location.href = "file:///D:/OFFICIAL/Website/Project/04%20Windows%20XP/index.html";
+    }
+    else {
+        var Error = document.getElementById("error");
+        Error.style.display = "block";
+        setTimeout(() => {
+            Error.style.display = "none";
+            }, 3000);
+    }
+}
+
+function normalMode(){
+    var Error = document.getElementById("error");
+    if (Error.style.display === "block"){
+        Error.style.display = "none";
+    }
 }
